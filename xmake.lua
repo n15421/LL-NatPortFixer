afea1be5d3c60016f1f1c88f5a3a6e4d2526800c
n15421 <119112174+n15421@users.noreply.github.com>
2025-02-24 21:42:26 +0800

    feat:adapt 1.21.60

--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,9 @@ add_rules("mode.debug", "mode.release")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("miracleforest-repo https://github.com/MiracleForest/xmake-repo.git")
 
-add_requires("levilamina 1.0.1", {configs = {target_type = "server"}})
+add_requires("levilamina 1.1.0-rc.1", {configs = {target_type = "server"}})
 add_requires("levibuildscript 0.3.0")
-add_requires("ilistenattentively 0.2.3")
+add_requires("ilistenattentively 0.3.0")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
